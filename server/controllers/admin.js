fa1013b41b567f350a567c080b1f7f5b4bb5d65d
chinonso Wiliiams <williamschinonso11gmail.com>
2020-04-08 10:39:49 +0100

    Create admin.js
    create admin registration function

---
+++ server/controllers/admin.js
@@ -0,0 +1,39 @@
+import passwordHash from 'password-hash';
+import adminSchema from '../db/models/admin';
+
+const Admin = {};
+
+// register admin
+Admin.Register = async (req, res) => {
+  const data = req.body;
+
+  if (!data.email || !data.password) {
+    return res.json({
+      success: false,
+      message: 'Please provide required fields',
+    });
+  }
+
+  try {
+    const hashPassword = passwordHash.generate(data.password); // encrypt admin password
+    data.password = hashPassword;
+    // console.log(data.password);
+    const result = await adminSchema.create(data);
+    if (!result) {
+      return res.json({
+        success: false,
+        message: 'error, while trying to create admin',
+      });
+    }
+
+    return res.json({ success: true, message: 'admin created successfully' });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: 'Error occurred',
+      error,
+    });
+  }
+};
+
+module.exports = Admin;
